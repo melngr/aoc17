91032e6b37ebbc1133226563cd9bef44bee37e6c
Griffin Melnick <melnig@users.noreply.github.com>
2017-12-02 17:11:31 -0500

    Completed day two.

---
+++ 17/day02.py
@@ -0,0 +1,112 @@
+'''
+Author:     Griffin Melnick, melnick.griffin@gmail.com
+File:       day02.py
+Purpose:    Advent of Code 2017, Day 2
+            Pulls in command line input of 'a' or 'b' to represent which set of
+            functions to call based on part.
+'''
+
+import itertools
+import sys
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+
+'''
+File reader.
+:requires:  file 'day02-input.txt' to exist in same directory
+:throws:    IOError, if file cannot be opened
+:returns:   all info from 'day02-input.txt' as a list of lists
+'''
+def read_spreadsheet():
+    try:
+        f = open("day02-input.txt", 'r')
+        spreadsheet = []
+        for line in f:
+            spreadsheet.append( line.strip().split(' ') )
+        f.close()
+    except IOError as err:
+        raise IOError( err )
+
+    return spreadsheet
+
+'''
+List conversion from string to int.
+:param:     spreadsheet, a list of list with strings
+:requires:  spreadsheet != None
+:throws:    ValueError, if list has illegal letter
+:returns:   new list of list with ints
+'''
+def convert(spreadsheet):
+    try:
+        int_spreadsheet = [list(map(int, x)) for x in spreadsheet]
+    except ValueError as err:
+        raise ValueError( err )
+
+    return int_spreadsheet
+
+'''
+Checksum finding method using max and min of each line.
+:param:     spreadsheet, a list of lists with ints
+:requires:  spreadsheet != None
+:returns:   checksum value of spreadsheet using method from part 'a'
+'''
+def total_checksum(spreadsheet):
+    checksum = 0
+    for vals in spreadsheet:
+        checksum += ( max(vals, default = 0) - min(vals, default = 0) )
+    return checksum
+
+'''
+Checksum finding method using evenly divisible numbers.
+:param:     spreadsheet, a list of lists with ints
+:requires:  spreadsheet != None
+:returns:   checksum value of spreadsheet using method from part 'b'
+'''
+def even_checksum(spreadsheet):
+    checksum = 0
+    for vals in spreadsheet:
+        for x, y in itertools.combinations( sorted(vals), 2 ):
+            if ( y % x == 0 ):
+                checksum += ( y // x )
+
+    return checksum
+
+# --------------------------------------------------------------
+
+'''
+Runs part 'a' appropriate functions and prints result.
+'''
+def part_a():
+    spreadsheet = convert( read_spreadsheet() )
+    sum = total_checksum(spreadsheet)
+    print( "The checksum is {0}.".format(sum) )
+
+'''
+Runs part 'b' appropriate functions and prints result.
+'''
+def part_b():
+    spreadsheet = convert( read_spreadsheet() )
+    sum = even_checksum(spreadsheet)
+    print( "The checksum is {0}.".format(sum) )
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+
+if __name__ == "__main__":
+    if ( len(sys.argv) == 2 ):
+        day = sys.argv[1]
+        if ( day.strip() == 'a' ):
+            part_a()
+
+        elif ( day.strip() == 'b' ):
+            part_b()
+
+        else:
+            sys.stderr.write( "USAGE: invalid argument\n" )
+
+    else:
+        if ( len(sys.argv) < 2 ):
+            sys.stderr.write( "USAGE: too few args\n" )
+        else:
+            sys.stderr.write( "USAGE: too many args\n" )
