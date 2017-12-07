ec60a492bec2f7dbe3b405257a9033c223315c81
Griffin Melnick <melnig@users.noreply.github.com>
2017-12-07 00:53:33 -0500

    Restructured files.
    Opted to move each year of Advent of Code to a separate repository.
    Restructured files to be more user-friendly.

---
+++ day01.py
@@ -0,0 +1,118 @@
+'''
+Author:     Griffin Melnick, melnick.griffin@gmail.com
+File:       day01.py
+Purpose:    Advent of Code 2017, Day 1
+            Pulls in command line input of 'a' or 'b' to represent which set of
+            functions to call based on part.
+'''
+
+import sys
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+
+'''
+File reader.
+:requires:  file 'day01-input.txt' to exist in same directory
+:throws:    IOError, if file cannot be opened
+:returns:   all info from 'day01-input.txt' as a string
+'''
+def read_captcha():
+    try:
+        f = open("day01-input.txt", 'r')
+        captcha = f.read().strip()
+        f.close()
+    except IOError as err:
+        raise IOError( err )
+
+    return captcha
+
+'''
+Next step summing method.
+:param:     captcha, the string read in using method 'read_captcha()'
+:throws:    IndexError, if attempt to access value is outside of list
+:returns:   sum using method defined in part 'a'
+'''
+def next_sum(captcha):
+    total = 0
+
+    for i in range(0, len(captcha)):
+        if ( i != (len(captcha) - 1) ):
+            j = i + 1
+        else:
+            j = 0
+
+        try:
+            at_i = captcha[i]
+            at_j = captcha[j]
+        except IndexError as err:
+            raise IndexError( err )
+
+        if ( at_i == at_j ):
+            total += int(at_i)
+
+    return total
+
+'''
+Half circle summing method.
+:param:     captcha, the string read in using method 'read_captcha()'
+:throws:    IndexError, if attempt to access value is outside of list
+:returns:   sum using method defined in part 'b'
+'''
+def half_sum(captcha):
+    total = 0
+    full = len(captcha)
+    half = int(full / 2)
+
+    for i in range(0, full):
+        j = ((i + half) % full)
+
+        try:
+            at_i = captcha[i]
+            at_j = captcha[j]
+        except IndexError as err:
+            raise IndexError( err )
+
+        if ( at_i == at_j ):
+            total += int(at_i)
+
+    return total
+
+# --------------------------------------------------------------
+
+'''
+Runs part 'a' appropriate functions and prints result.
+'''
+def part_a():
+    captcha = read_captcha()
+    total = next_sum(captcha)
+    print( "The CAPTCHA sum is {0}.".format(total) )
+
+'''
+Runs part 'b' appropriate functions and prints result.
+'''
+def part_b():
+    captcha = read_captcha()
+    total = half_sum(captcha)
+    print( "The CAPTCHA sum is {0}.".format(total) )
+
+# --------------------------------------------------------------
+# --------------------------------------------------------------
+
+if __name__ == "__main__":
+    if ( len(sys.argv) == 2 ):
+        day = sys.argv[1]
+        if ( day.strip() == 'a' ):
+            part_a()
+
+        elif ( day.strip() == 'b' ):
+            part_b()
+
+        else:
+            sys.stderr.write( "USAGE: invalid argument" )
+
+    else:
+        if ( len(sys.argv) < 2 ):
+            sys.stderr.write( "USAGE: too few args" )
+        else:
+            sys.stderr.write( "USAGE: too many args" )
